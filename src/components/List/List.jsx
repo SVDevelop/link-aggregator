dd346cb48dbd86b9ef66e6bdbe7d359ef475240b
svdevelop <verstka.sv@mail.ru>
2023-02-18 11:14:06 +0300

    addApi

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,7 +1,21 @@
-export const List =()=>{
+import {useState, useEffect} from 'react'
+
+export const List = () => {
     const url = "https://random-data-api.com/api/v2/users?size=3"
+    const [data, setData] = useState([])
+    const [value, setValue] = useState(5)
+
+    useEffect(() => {
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setData(data))
+    }, [value])
+    console.log(data)
     return (
-        <><input type='text' placeholder='Поиск' />
+        <>
+            <input type='text' placeholder='Поиск' value={value} onChange={({target}) => {
+                setValue(target.value)
+            }} />
         </>
     
     )
