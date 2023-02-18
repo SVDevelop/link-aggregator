c851232e8a0c780557c49200633e7f3da513c2a4
darf1988 <57325702+darf1988@users.noreply.github.com>
2023-02-18 11:39:25 +0300

    data

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,12 +1,12 @@
 import {useState, useEffect} from 'react'
 
 export const List = () => {
-    const url = "https://random-data-api.com/api/v2/users?size=3"
+    const url = "https://random-data-api.com/api/v2/users?size="
     const [data, setData] = useState([])
     const [value, setValue] = useState(5)
 
     useEffect(() => {
-        fetch(url)
+        fetch(`${url+value}`)
         .then(res => res.json())
         .then(data => setData(data))
     }, [value])
@@ -16,6 +16,11 @@ export const List = () => {
             <input type='text' placeholder='Поиск' value={value} onChange={({target}) => {
                 setValue(target.value)
             }} />
+            <ul>
+                {data.map(({address})=>(
+                  <li>{address.city}</li>  
+                ))}
+            </ul>
         </>
     
     )
