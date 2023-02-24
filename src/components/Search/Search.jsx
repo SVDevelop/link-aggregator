097f0a7330a214b71e52e0c5cd92f97881266984
svdevelop <verstka.sv@mail.ru>
2023-02-24 20:11:13 +0300

    addBtn

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,11 +2,15 @@ import {useState, useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import { selectSearch, setSearch, setSearchData } from '../../store/searchSlice';
 
+import clsx from 'clsx';
+import styles from './Search.module.scss'
+
 const url = "https://random-data-api.com/api/v2/users?size="
 
 const Search = () => {
     const {data, value} = useSelector(selectSearch)
     const dispatch = useDispatch()
+    const [hidden, setHidden] = useState(false)
 
     useEffect(() => {
         fetch(`${url}${value}`)
@@ -15,12 +19,16 @@ const Search = () => {
     }, [value])
     console.log('value: ',data)
     return (
-        <>
-            <input type='number' placeholder='Поиск' value={value} onChange={({target}) => {
+        <div className={clsx(styles.wrapper)}>
+            <input className={clsx(styles.input, {[styles.hidden]: hidden})} type='number' placeholder='Поиск' value={value} onChange={({target}) => {
                 dispatch(setSearch({value: target.value}))
             }} />
-        </>
+            <button className={clsx(styles.btn)} onClick={() => {
+                setHidden(x => !x)
+            }}>btn</button>
+        </div>
     );
 };
 
 export default Search;
+
